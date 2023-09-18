9c78b75f6b052990dda881d7f3be0c1eebf839fd
khalid999devs <khalidahammeduzzal@gmail.com>
2023-09-18 15:58:37 +0600

    done

--- threejsbasics/app.js
+++ threejsbasics/app.js
@@ -21,51 +21,92 @@ window.addEventListener('resize', (_) => {
   camera.updateProjectionMatrix();
 });
 
+let rayCaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshLambertMaterial({ color: 0xffcc00 });
-const box = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshLambertMaterial({ color: 0xf7f7f7 });
+// const box = new THREE.Mesh(geometry, material);
 // box.position.x = -2;
 // box.position.y = 2;
 // box.position.z = -2;
+
+// const geometry1 = new THREE.BoxGeometry(1, 1, 1);
+// const material1 = new THREE.MeshLambertMaterial({ color: 0xffcc00 });
+// const box1 = new THREE.Mesh(geometry1, material1);
+
+// box1.position.y = 2;
+// scene.add(box1)
 
 //shorthand
 // box.position.set(2, 2, -2);
 // box.rotation.set(45, 0, 0);
 // box.scale.set(1, 2, 1);
 
-scene.add(box);
+// scene.add(box);
 
-let light = new THREE.PointLight(0xffffff, 1, 500);
-light.position.set(10, 0, 25);
+let meshX = -10;
+for (let i = 0; i < 15; i++) {
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = (Math.random() - 0.5) * 10;
+  mesh.position.y = (Math.random() - 0.5) * 10;
+  mesh.position.z = (Math.random() - 0.5) * 10;
+  scene.add(mesh);
+  meshX += 1;
+}
+
+let light = new THREE.PointLight(0xffffff, 1, 1000);
+light.position.set(0, 0, 0);
 scene.add(light);
+
+let light1 = new THREE.PointLight(0xffffff, 2, 500);
+light1.position.set(0, 0, 25);
+scene.add(light1);
 
 let render = () => {
   requestAnimationFrame(render);
-  box.rotation.x += 0.02;
-  box.rotation.y += 0.01;
+  // box.rotation.x += 0.02;
+  // box.rotation.y += 0.01;
 
-  box.scale.x -= 0.01;
+  // box.scale.x -= 0.01;
 
   renderer.render(scene, camera);
 };
 
+function onMouseMove(event) {
+  event.preventDefault();
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  rayCaster.setFromCamera(mouse, camera);
+
+  const intersects = rayCaster.intersectObjects(scene.children);
+
+  for (let i = 0; i < intersects.length; i++) {
+    const tl = gsap.timeline();
+    tl.to(intersects[i].object.scale, {
+      x: 2,
+      ease: Expo.easeout,
+      duration: 1,
+    });
+    tl.to(intersects[i].object.scale, {
+      x: 0.5,
+      ease: Expo.easeout,
+      duration: 0.5,
+    });
+    tl.to(intersects[i].object.position, {
+      x: 2,
+      ease: Expo.easeout,
+      duration: 0.5,
+    });
+    tl.to(
+      intersects[i].object.rotation,
+      { y: Math.PI * 0.5, ease: Expo.easeout, duration: 0.5 },
+      '=-1.5'
+    );
+  }
+}
+
 render();
-// Create a cube and add it to the scene
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
 
-// // Animation function
-// const animate = () => {
-//   requestAnimationFrame(animate);
-
-//   // Rotate the cube
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-
-//   renderer.render(scene, camera);
-// };
-
-// // Start the animation
-// animate();
+document.body.addEventListener('mousemove', onMouseMove);
